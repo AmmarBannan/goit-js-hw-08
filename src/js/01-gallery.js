a429f945c36b54b3cedd57103b65cfd1c956d786
ammar <ammarabannan@gmail.com>
2022-09-18 23:02:58 +0300

    test3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,29 +4,20 @@ import { galleryItems } from "./gallery-items";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = document.querySelector(".gallery");
-const gallaryCardsItems = createGallaryCardsItems(galleryItems);
-
-gallery.insertAdjacentHTML("beforeend", gallaryCardsItems);
-
-function createGallaryCardsItems(galleryItems) {
-  return galleryItems
-    .map(({ original, preview, description }) => {
-      return `
-        <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-        />
-        </a>
-    </div>
-    `;
-    })
-    .join("");
-}
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML= galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                    </a>
+                </div>`;
+      })
+      .join('');
 
 new SimpleLightbox(".gallery a", {
   captionsData: "alt",
